7f383bc9c36ae43e70edde850a28004ace148a32
Martin Marconcini <marconcini@gmail.com>
2018-11-27 14:59:18 +0100

    Add more android paths.

--- androidpath.sh
+++ androidpath.sh
@@ -4,10 +4,14 @@
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home
 JAVA_HOME=$(/usr/libexec/java_home) PATH=$PATH:$JAVA_HOME/bin export JAVA_HOME
 
-# Android path and tools
+# -------------------------------------------
+# ---- Add Android SDK tools and platform-tools to path —
+# -------------------------------------------
 export "ANDROID_HOME=/Users/martin/Develop/SDK/android-sdk-macosx"
 export "ANDROID_NDK_HOME=/Users/martin/dev/speedtestnet-android/toolchain/ndk"
 export PATH=$PATH:/opt/gradle-2.0/bin
+export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/emulator"
+
 
 # Gradle Settings
 export GRADLE_OPTS=-Xmx1024m
