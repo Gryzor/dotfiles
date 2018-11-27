9b68c4a82bdca9ea469097aec1d9b291caf8b7ba
Martin Marconcini <marconcini@gmail.com>
2018-11-27 14:42:14 +0100

    Add Dotfiles

---
+++ installSymlinks.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitexcludes ~/.gitexcludes
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
